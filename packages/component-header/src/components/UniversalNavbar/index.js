050363ca736da250694aa411573ee4ba69b58b5c
Aneesa Khanam <akhanam@asu.edu>
2026-07-21 17:19:43 +0530

    search updated

--- packages/component-header/src/components/UniversalNavbar/index.js
+++ packages/component-header/src/components/UniversalNavbar/index.js
@@ -1,114 +1,104 @@
 // @ts-check
-import React, { forwardRef } from "react";
-
 import { trackGAEvent } from "../../../../../shared";
+import React from "react";
+
 import { useAppContext } from "../../core/context/app-context";
-import { useIsMobile } from "../../core/hooks/isMobile";
+import { CLASS_NAMES, buildClassName } from "../../core/constants/classNames";
 import { Wrapper } from "./index.styles";
 import { Login } from "./Login";
 import { Search } from "./Search";
+import { useIsMobile } from "../../core/hooks/isMobile";
 
 const DEFAULT_GA_EVENT = {
-  // https://www.dropbox.com/scl/fo/gmkapav1avulctkge0w9q/AFF5UCx0jwCOHPhM8ZoaKOg/About%20ASU%20Sun%20Devil%20Athletics%20%20%20ASU%20Sun%20Devil%20Athletics.pdf?rlkey=le42w6mnh6hukls733k3ej41c&e=3&dl=0
   event: "link",
   action: "click",
   name: "onclick",
-  type: "internal link",
   region: "navbar",
+  type: "internal link",
   section: "topbar",
-  text: "asu home",
-  component: "text",
+  text: "",
 };
 
-function getURL() {
-  try {
-    const URL = window.location.href;
-    return URL;
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error(error);
-    return "";
-  }
-}
-
-const UniversalNavbar = forwardRef((_props, ref) => {
-  const { breakpoint, universalNavbar } = useAppContext();
+const UniversalNavbar = () => {
+  const { breakpoint } = useAppContext();
   const isMobile = useIsMobile(breakpoint);
-  const isDesktop = !isMobile;
+
+  function getURL() {
+    try {
+      const URL = window.location.href;
+      return URL;
+    } catch (error) {
+      console.error(error);
+      return "";
+    }
+  }
+
+  const universalNavLinks = [
+    {
+      className: buildClassName(
+        CLASS_NAMES.NAV_LINK,
+        CLASS_NAMES.VISUALLY_HIDDEN_FOCUSABLE
+      ),
+      href: "#skip-to-content",
+      text: "Skip to main content",
+    },
+    {
+      className: buildClassName(
+        CLASS_NAMES.NAV_LINK,
+        CLASS_NAMES.VISUALLY_HIDDEN_FOCUSABLE
+      ),
+      href: `https://accessibility.asu.edu/report#a11yref=${getURL()}`,
+      text: "Report an accessibility problem",
+    },
+    {
+      className: CLASS_NAMES.NAV_LINK,
+      href: "https://asu.edu",
+      text: "ASU Home",
+    },
+    {
+      className: CLASS_NAMES.NAV_LINK,
+      href: "https://my.asu.edu",
+      text: "My ASU",
+    },
+    {
+      className: CLASS_NAMES.NAV_LINK,
+      href: "https://www.asu.edu/academics/colleges-schools",
+      text: "Colleges and Schools",
+    },
+  ];
 
   return (
     <Wrapper
       // @ts-ignore
       breakpoint={breakpoint}
-      className="universal-nav"
+      className={CLASS_NAMES.UNIVERSAL_NAV}
       data-testid="universal-navbar"
       data-elastic-exclude="data-elastic-exclude"
-      universalNavbar={universalNavbar}
-      ref={ref}
-      style={{ pointerEvents: "auto" }}
     >
       <div className="container-xl">
-        <div className="header-top">
-          <nav className="nav" aria-label="ASU">
-            <div className="links-container">
-              <a
-                className="nav-link visually-hidden-focusable"
-                href="#skip-to-content"
-              >
-                Skip to main content
-              </a>
-              <a
-                className="nav-link visually-hidden-focusable"
-                href={`https://accessibility.asu.edu/report?a11yref=${getURL()}`}
-              >
-                Report an accessibility problem
-              </a>
-
-              {isDesktop && (
-                <>
-                  {universalNavbar?.renderStart?.({ isMobile, isDesktop })}
-                  <div className="links-whitespace" />
-                </>
-              )}
-
-              <a
-                className="nav-link"
-                href="https://asu.edu"
-                onFocus={() =>
-                  trackGAEvent({ ...DEFAULT_GA_EVENT, text: "asu home" })
-                }
-              >
-                ASU Home
-              </a>
-              <a
-                className="nav-link"
-                href="https://my.asu.edu"
-                onFocus={() =>
-                  trackGAEvent({ ...DEFAULT_GA_EVENT, text: "my asu" })
-                }
-              >
-                My ASU
-              </a>
-              <a
-                className="nav-link"
-                href="https://www.asu.edu/academics/colleges-schools"
-                onFocus={() =>
-                  trackGAEvent({
-                    ...DEFAULT_GA_EVENT,
-                    text: "colleges and schools",
-                  })
-                }
-              >
-                Colleges and Schools
-              </a>
+        <div className={CLASS_NAMES.HEADER_TOP}>
+          <nav className="nav" aria-label="ASU Global">
+            <div className={CLASS_NAMES.LINKS_CONTAINER}>
+              {universalNavLinks.map(link => (
+                <a
+                  key={link.href}
+                  className={link.className}
+                  href={link.href}
+                  onClick={() =>
+                    trackGAEvent({ ...DEFAULT_GA_EVENT, text: link.text })
+                  }
+                >
+                  {link.text}
+                </a>
+              ))}
               <Login />
             </div>
-            <Search />
+            {!isMobile && <Search />}
           </nav>
         </div>
       </div>
     </Wrapper>
   );
-});
+};
 
 export { UniversalNavbar };
